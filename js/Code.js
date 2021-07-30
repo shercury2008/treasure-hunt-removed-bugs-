863ae49bdac2bfd526e81e7711ca10e84572ea7b
shercury2008 <82042102+shercury2008@users.noreply.github.com>
2021-07-30 23:25:19 +0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,17 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    text("A T A D A S B A", 100,360)
+    fill("lightgreen")
+    text("Hint: Specific conditions !!", 700,170)
+    text("C U T N I F O N.", 700,150)
+    fill("lightblue")
+    text("Hint: Stores information !!", 100,380)
+}
